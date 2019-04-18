3e1665314b931ea63a45d2ea468655354f987e73
a.marcomini <devnull@localhost>
2019-04-18 14:06:55 +0200

    inserimento classe di costanti e creato RawMessageRepository

---
+++ src/main/java/it/bologna/ausl/shpeck/service/constants/ApplicationConstant.java
@@ -0,0 +1,18 @@
+package it.bologna.ausl.shpeck.service.constants;
+
+/**
+ *
+ * @author spritz
+ */
+public class ApplicationConstant {
+    
+    public static final int MESSAGE_POLICY_NONE = 0;
+    public static final int MESSAGE_POLICY_BACKUP = 1;
+    public static final int MESSAGE_POLICY_DELETE = 2;
+    
+    public static final String ORPHAN_KEY = "orphan";
+    public static final String OK_KEY = "ok";
+    
+    
+    
+}
