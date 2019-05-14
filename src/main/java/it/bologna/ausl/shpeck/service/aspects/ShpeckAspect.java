7122d9e6379618fc0e578842b38548b077f9f19e
l.salomone <devnull@localhost>
2019-05-14 10:49:34 +0200

    AOP commentato

--- src/main/java/it/bologna/ausl/shpeck/service/aspects/ShpeckAspect.java
+++ src/main/java/it/bologna/ausl/shpeck/service/aspects/ShpeckAspect.java
@@ -13,33 +13,33 @@ import org.springframework.stereotype.Component;
  *
  * @author Salo
  */
-@Aspect
-@Component
-public class ShpeckAspect {
-    
-    @Pointcut("execution(* it.bologna.ausl.shpeck.service..*.*(..))")
-    private void anyOperation() {}
-    
-    @Pointcut("execution(* it.bologna.ausl.shpeck.serviceaspects..*.*(..))")
-    private void aspectOperation() {}
-    
-    @Pointcut("anyOperation() && !aspectOperation()")
-    private void operationToLog() {}
-    
-    @Before(value = "operationToLog()")
-    private void logTheEnteringOfMethod(JoinPoint joinPoint){
-        System.out.println("[**] ENTERING: " + joinPoint.getSignature());
-        Object[] argObjects = joinPoint.getArgs();
-        for (Object argObject : argObjects) {
-            System.out.println("\t" + argObject.getClass() + ": " + argObject.toString());
-        }
-        
-    }
-    
-    @AfterReturning(pointcut = "operationToLog()", returning="retVal")
-    private void logTheReturningOfMethod(JoinPoint joinPoint, Object retVal){
-        System.out.println("[<=] RETURNING: " + joinPoint.getSignature() + (retVal != null ? "\t*retVal: " + retVal.toString() : ""));
-//        if(retVal != null)
-//            System.out.println("\t[<=] *retVal: " + retVal.toString());
-    }
-}
+//@Aspect
+//@Component
+//public class ShpeckAspect {
+//    
+//    @Pointcut("execution(* it.bologna.ausl..*.*(..))")
+//    private void anyOperation() {}
+//    
+//    @Pointcut("execution(* it.bologna.ausl.shpeck.serviceaspects..*.*(..))")
+//    private void aspectOperation() {}
+//    
+//    @Pointcut("anyOperation() && !aspectOperation()")
+//    private void operationToLog() {}
+//    
+//    @Before(value = "operationToLog()")
+//    private void logTheEnteringOfMethod(JoinPoint joinPoint){
+//        System.out.println("[**] ENTERING: " + joinPoint.getSignature());
+//        Object[] argObjects = joinPoint.getArgs();
+//        for (Object argObject : argObjects) {
+//            System.out.println("\t" + argObject.getClass().getTypeName() + ": " + argObject.toString());
+//        }
+//        
+//    }
+//    
+//    @AfterReturning(pointcut = "operationToLog()", returning="retVal")
+//    private void logTheReturningOfMethod(JoinPoint joinPoint, Object retVal){
+//        System.out.println("[<=] RETURNING: " + joinPoint.getSignature() + (retVal != null ? "\t*retVal: " + retVal.toString() : ""));
+////        if(retVal != null)
+////            System.out.println("\t[<=] *retVal: " + retVal.toString());
+//    }
+//}
