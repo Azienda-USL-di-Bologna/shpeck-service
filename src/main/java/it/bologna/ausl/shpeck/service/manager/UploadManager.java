9deaaa9043840b538affadbb82c0cb7f45fdb9cd
l.salomone <devnull@localhost>
2019-06-17 16:29:04 +0200

    due log sul upload Manager

--- src/main/java/it/bologna/ausl/shpeck/service/manager/UploadManager.java
+++ src/main/java/it/bologna/ausl/shpeck/service/manager/UploadManager.java
@@ -52,37 +52,53 @@ public class UploadManager {
     @Transactional(rollbackFor = Throwable.class, propagation = Propagation.REQUIRES_NEW)
     public void manage(UploadQueue messageToStore) throws ShpeckServiceException {
         try {
+            log.info("Entrato in manage: carico i parametri azienda");
             // ottieni parametri di mongo di un specifico ambiente guardando l'azienda associata alla pec
             AziendaParametriJson aziendaParams = AziendaParametriJson.parse(objectMapper, messageToStore.getIdRawMessage().getIdMessage().getIdPec().getIdAziendaRepository().getParametri());
+
+            log.info("aziendaParams " + aziendaParams.toString());
             AziendaParametriJson.MongoParams mongoParams = aziendaParams.getMongoParams();
 
+            log.info("setto lo storageStrategy");
             // inizializzazione del context storage
             storageContext.setStorageStrategy(new MongoStorage(mongoParams.getConnectionString(), mongoParams.getRoot()));
 
+            log.info("Fattio lo store del message");
             // esegue lo store del messaggio e ritorna l'oggetto con le proprietà settate (es: uuid, path, ...)
             UploadQueue objectUploaded = storageContext.store(inboxForlder, messageToStore);
 
+            log.info("Carico il messaggio (optional)");
             // ottieni in messaggio associato al contenuto appena caricato
             Optional<Message> message = messageRepository.findById(messageToStore.getIdRawMessage().getIdMessage().getId());
 
+            log.info("verifico presenza del messaggio");
             // se messaggio è presente, si settano le proprietà relative al messaggio appena salvato nello storage
             if (message.isPresent()) {
+                log.info("messaggio presente");
                 Message messageToUpdate = message.get();
+                log.info("messaggio setto il uuid");
                 messageToUpdate.setUuidRepository(objectUploaded.getUuid());
+                log.info("messaggio setto il path");
                 messageToUpdate.setPathRepository(objectUploaded.getPath());
                 messageToUpdate.setName(objectUploaded.getName());
-                // update del mesaggio con i nuovi parametri                    
+                log.info("--> " + messageToUpdate.toString());
+                // update del mesaggio con i nuovi parametri   
+                log.info("salvo");
                 messageRepository.save(messageToUpdate);
                 if (objectUploaded.getUuid() != null) {
+                    log.info("setto uploaded true");
                     // set come file già trattato nella tabella upload_queue
                     objectUploaded.setUploaded(Boolean.TRUE);
+                    log.info("aggiorno objectUploaded");
                     uploadQueueRepository.save(objectUploaded);
                 }
             }
+
         } catch (Throwable e) {
             log.error("errore nell'upload del messaggio con id su upload_queue: " + messageToStore.getId());
             log.error("con errore: " + e);
             throw new ShpeckServiceException("errore nell'upload del messaggio");
         }
+        log.info("[manage finito]");
     }
 }
