72e8814032cf4f0550acbc14e62257cef51802aa
a.marcomini <devnull@localhost>
2019-06-20 10:56:23 +0200

    gestione externalId

--- src/main/java/it/bologna/ausl/shpeck/service/manager/RegularMessageStoreManager.java
+++ src/main/java/it/bologna/ausl/shpeck/service/manager/RegularMessageStoreManager.java
@@ -25,49 +25,50 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class RegularMessageStoreManager extends StoreManager {
-
+    
     private static final Logger log = LoggerFactory.getLogger(RegularMessageStoreManager.class);
-
+    
     private MailMessage mailMessage;
     private Pec pec;
     private Outbox outbox; // Viene valorizzato solo nel caso di SMTP per un messaggio spedito da Pico.
 
     public RegularMessageStoreManager() {
     }
-
+    
     public Pec getPec() {
         return pec;
     }
-
+    
     public void setPec(Pec pec) {
         this.pec = pec;
     }
-
+    
     public MailMessage getMailMessage() {
         return mailMessage;
     }
-
+    
     public void setMailMessage(MailMessage mailMessage) {
         this.mailMessage = mailMessage;
     }
-
+    
     public Outbox getOutbox() {
         return outbox;
     }
-
+    
     public void setOutbox(Outbox outbox) {
         this.outbox = outbox;
     }
-
+    
     @Transactional(rollbackFor = Throwable.class, propagation = Propagation.REQUIRED)
     public StoreResponse store() throws MailMessageException, StoreManagerExeption, ShpeckServiceException {
-
+        
         log.info("--- inizio RegularMessageStoreManager.store() ---");
         Message regularMessage = createMessageForStorage((MailMessage) mailMessage, pec);
         regularMessage.setIdApplicazione(getApplicazione());
         regularMessage.setIdOutbox(((outbox == null) || (outbox.getId() == null) ? null : outbox.getId()));
         regularMessage.setMessageType(Message.MessageType.MAIL);
         regularMessage.setIsPec(Boolean.FALSE);
+        regularMessage.setExternalId(((outbox == null) || (outbox.getExternalId() == null) ? null : outbox.getExternalId()));
         Message messagePresentInDB = getMessageFromDb(regularMessage);
         if (messagePresentInDB == null) {
             regularMessage = storeMessage(regularMessage);
@@ -87,7 +88,7 @@ public class RegularMessageStoreManager extends StoreManager {
         HashMap mapMessagesAddress = upsertAddresses(mailMessage);
         log.debug("salvo/aggiorno sulla cross il regular message e indirizzi");
         storeMessagesAddresses(regularMessage, mapMessagesAddress);
-
+        
         return new StoreResponse(ApplicationConstant.OK_KEY, mailMessage, regularMessage);
     }
 }
