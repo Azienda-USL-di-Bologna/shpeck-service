7e1a1cce9cfc15754ee5a52ee7a51698b93d1122
a.marcomini <devnull@localhost>
2019-10-01 10:35:48 +0200

    fix su upload queue per controllare che la pec abbia anche id_repository settato

--- src/main/java/it/bologna/ausl/shpeck/service/worker/UploadWorker.java
+++ src/main/java/it/bologna/ausl/shpeck/service/worker/UploadWorker.java
@@ -153,6 +153,10 @@ public class UploadWorker implements Runnable {
             log.info("oggetto Message creato");
             Pec p = pecRepository.findById(m.getIdPec().getId()).get();
             log.info("oggetto Pec creato");
+            if (p.getIdAziendaRepository() == null) {
+                log.warn("la pec con indirizzo " + p.getIndirizzo() + " non ha un id_repository associato");
+                continue;
+            }
             Azienda a = aziendaRepository.findById(p.getIdAziendaRepository().getId()).get();
             log.info("oggetto Azienda creato");
             p.setIdAziendaRepository(a);
