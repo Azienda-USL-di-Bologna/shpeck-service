4448bafbc6e1f9c6e02b0c4556af356bcb7ce410
mussi <devnull@localhost>
2021-03-04 17:27:14 +0100

    Modificata la query per prendere valori inferiori di 3 ore e non di un giorno

--- src/main/java/it/bologna/ausl/shpeck/service/repository/RecepitRepository.java
+++ src/main/java/it/bologna/ausl/shpeck/service/repository/RecepitRepository.java
@@ -15,7 +15,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "recepit", path = "recepit", exported = false, excerptProjection = RecepitWithPlainFields.class)
 public interface RecepitRepository extends JpaRepository<Recepit, Integer>{
     
-    @Query(value = "select cast (id as int) id from shpeck.recepits r, shpeck.messages m2 " +
+    @Query(value = "select cast (r.id as int) id from shpeck.recepits r, shpeck.messages m2 " +
                     "where r.id = m2.id " +
                     "and r.recepit_type = 'ACCETTAZIONE' " +
                     "and m2.message_type = 'RECEPIT' " +
